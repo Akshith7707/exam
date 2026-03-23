1e1c0d5f496f9c25af565893e9ff4bf8d2e577c3
245123751013-design <your@email.com>
2026-03-23 14:59:23 +0530

    hello

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+	static public void main(String...args)
+	{
+		System.out.println("Hello World!")
+		System.out.println("This is line 2");
+	        System.out.println("This is line 3");
+
+
+
+	}
+}
